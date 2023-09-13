63fd463b9680caba415dc3230bba1a3164662eac
Naomichela <naomichelagatmasipai@gmail.com>
2023-09-13 11:59:42 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,4 @@
 #include "main.h"
-/**
- * add - adds two integers and returnsthe result
- *
- * @x: number being added
- *
- * @y: number being added
- *
- * Return: Always 0.
- *
- */
-int add(int x, int y)
-{
-	return (x + y);
-}
-
-
-Task 11
-
-#include "main.h"
 #include <stdio.h>
 /**
  * print_to_98 - prints all natural numbers from n to 98
