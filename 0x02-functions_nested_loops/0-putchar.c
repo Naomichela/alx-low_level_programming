ab575ee49b6603c5a0c5d12d0d9158485c59ccd1
Naomichela <naomichelagatmasipai@gmail.com>
2023-09-13 11:33:42 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - this is the main function
+ * main - Entry point
  *
- * Return - always 0
+ * Return: Always 0 (success)
  */
 
-int main() {
-    char text[] = "_putchar\n";
-    write(1, text, sizeof(text) - 1); // 1 is the file descriptor for stdout
-    return 0;
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
 }
